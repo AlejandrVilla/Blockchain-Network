284037eb2e54c588f5d0010bfdf2671e16401010
Avherrera <avillah@unsa.edu.pe>
2023-11-23 11:12:18 -0500

    app gateway

--- asset-transfer-basic/application-gateway-go/assetTransfer.go
+++ asset-transfer-basic/application-gateway-go/assetTransfer.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	mspID        = "Org1MSP"
-	cryptoPath   = "../../test-network/organizations/peerOrganizations/org1.example.com"
+	cryptoPath   = "../../Rnetwork/organizations/peerOrganizations/org1.example.com"
 	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
 	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
 	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
